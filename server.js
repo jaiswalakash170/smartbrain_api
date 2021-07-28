c2f896f3f50a01867d1f0a0b4279bc15afff9090
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-29 00:38:05 +0530

    Implemented transaction using KNEX in /register endpoint to store user infor in users table and login info into login table

--- server.js
+++ server.js
@@ -75,19 +75,29 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {name, email, password} = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
-    db('users')
-        .returning('*')
-        .insert({
-            name: name,
-            email: email,
-            joined: new Date()
-        }).then(user => {
-            res.json(user[0]);
+    const hash = bcrypt.hashSync( password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .catch(err => res.status(400).json("Unable to register"));
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: loginEmail[0],
+                    joined: new Date()
+                })
+                .then(user => {
+                     res.json(user[0]);
+                })
+        })
+        .then(trx.commit)
+        .catch(err => res.status(400).json("trx roolback"));
+    })
 })
 
 app.get('/profile/:userId', (req, res) => {
