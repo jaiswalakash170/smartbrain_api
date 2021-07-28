46e5b13834be828f762b20384e57e064de4e69c4
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-28 12:35:09 +0530

    Added /, /signin, /register, /profile/.userId and /image endpoints

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log("This is working");
+})
+
+app.post('/signin', (req, res) => {
+    console.log("POST - SignIn");
+})
+
+app.post('/register', (req, res) => {
+    console.log("POST - Register");
+})
+
+app.get('/profile/.userId', (req, res) => {
+    console.log("GET - profile", userId);
+})
+
+app.put('/image', (req, res) => {
+    console.log("PUT - Image");
+})
+
 app.listen(3000, () => {
     console.log("App is running on port 3000");
 });
