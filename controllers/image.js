43e7e08c9b154d5b8437d61a742140ec7a017238
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-29 02:40:12 +0530

    Created new /imageurl endpoint to make call to ClariFai APIs

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,16 @@
+const Clarifai = require('clarifai');
+const clarifai_api_key = require('./../api_key');
+
+const app = new Clarifai.App(clarifai_api_key.clarifai_api_key);
+
+const handleAPICall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => res.json(data))
+    .catch(error => {
+        res.status(400).json("Unable to work with API");
+    });
+}
+
 const handleImage = (req, res, db) => {
     const {id} = req.body;
     db('users').where('id', '=', id).increment('entries', 1)
@@ -9,5 +22,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage : handleImage,
+    handleAPICall: handleAPICall
 }
--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.get('/profile/:userId', (req, res) => {profile.handleProfile(req, res, db)})
 
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
+app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)})
+
 app.listen(3001, () => {
     console.log("App is running on port 3001");
 });
